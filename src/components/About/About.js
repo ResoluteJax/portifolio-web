360593d7ecd5d8e34eaee909a723ff9a121f4602
ResoluteJax <otavio-henrique10@hotmail.com>
2025-04-15 10:25:37 -0300

    Feat: Complete portifolio section (About, Projects, Contact) and deploy

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 // src/components/About/About.js
 import React from 'react';
 import '../../assets/css/About.css';
-import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs, FaGitAlt } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs, FaGitAlt, FaDownload} from 'react-icons/fa';
 import { BsStars } from "react-icons/bs"; 
 
 
@@ -11,6 +11,9 @@ import { motion } from 'framer-motion';
 
 
 const About = React.forwardRef((props, ref) => {
+
+const cvFileName = "files/CV_Otavio_Henrique.pdf"; //arquivo do CV
+
   return (
     <motion.section
       ref={ref}
@@ -29,9 +32,10 @@ const About = React.forwardRef((props, ref) => {
            <img src={profileAboutPic} alt="Foto de Otávio Henrique" className="about-profile-pic"/>
         </div>
         <div className="about-text-container">
-           {/* ... parágrafos <p> ... */}
+           
             
-           <p>
+{/* ... parágrafos <p> ... */}
+<p>
   Olá! Sou Otávio Henrique, um entusiasta de tecnologia recém-formado em <strong>Análise e Desenvolvimento de Sistemas</strong> pela Universidade Veiga de Almeida, complementando formações técnicas em Informática e Programação. Minha paixão por resolver problemas e criar soluções me guiou naturalmente para o desenvolvimento de software.
 </p>
 
@@ -55,6 +59,16 @@ const About = React.forwardRef((props, ref) => {
   Fora do código, recarrego as energias assistindo animes, tocando violão ou com video games. Também valorizo a <strong>disciplina</strong> e o <strong>trabalho em equipe</strong>, que pratico em meu grupo de pedal e como Desbravador. Estou pronto e <strong>motivado</strong> para integrar uma equipe dinâmica, <strong>aprender</strong> com profissionais experientes e <strong>crescer</strong> junto com a empresa!
 </p>
 
+          <div className="cv-download-area"> {/* Div para ajudar no alinhamento/espaçamento */}
+          <a
+          // A construção do href continua a mesma
+          href={`${process.env.PUBLIC_URL}/${cvFileName}`}
+          download="CV_Otavio_Henrique.pdf"
+          className="cv-download-button"
+        >
+          <FaDownload /> Baixar Currículo
+        </a>
+          </div>
 
           {/* Seção de skills */}
           <div className="skills-section">
